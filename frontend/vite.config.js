887ee333433c6a645b6d561580015cc1412716d0
pruthvi <pruthvirajbarbole55@gmail.com>
2024-06-14 19:54:09 +0530

    Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    host:'0.0.0.0',
+    port:10000,
     proxy:{
-      '/api': 'http://localhost:8080',
+      '/api':{
+        target:'https://mern-stack-projecthospital-management.onrender.com',
+        changeOrigin:true,
+        secure:false
+      },
     },
   },
   plugins: [react()],
